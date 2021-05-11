b61416fc26a0c76e580729233b70b71509069545
Maria Belik <mashabelik@gmail.com>
2021-05-11 12:10:29 +0300

    Updates

--- main.9d87c0a16dc33046d83d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{L1EO:function(e,t,s){},QfWi:function(e,t,s){"use strict";s.r(t);s("L1EO");var c="light-theme",i="dark-theme",n=document.querySelector("#theme-switch-toggle"),o=document.querySelector("body");n.addEventListener("change",(function(e){n.checked?(o.classList.add(i),o.classList.remove(c)):(o.classList.remove(i),o.classList.add(c))}))}},[["QfWi",1]]]);
-//# sourceMappingURL=main.9d87c0a16dc33046d83d.js.map
